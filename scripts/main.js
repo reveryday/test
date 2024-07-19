c7aac4c494a2380ed686f50783cdfc10a42c9a89
reveryday <wenswuu@gmail.com>
2024-07-20 00:31:12 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
- document.querySelector("img").addEventListener("click", function () {
-     alert("别戳我，我怕疼。")
-})
+document.querySelector("img").addEventListener("click", function () {
+    alert("别戳我，我怕疼。");
+  });
