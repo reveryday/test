3d848624fad68d711ac074d6a8219a0daeaa9e59
reveryday <wenswuu@gmail.com>
2024-07-20 00:30:19 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-// document.querySelector("img").addEventListener("click", function () {
-//     alert("别戳我，我怕疼。");
-//   });
-let myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/zyx02.jpg") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/zyx02.jpg");
-  }
-};
+ document.querySelector("img").addEventListener("click", function () {
+     alert("别戳我，我怕疼。")
+})
